483deca504b4a0a2666e6f2be646ac8991c6acb4
nibelzin <live12luan@hotmail.com>
2025-09-01 16:15:22 -0300

    fix: note card overflow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import UserNote from '@/components/notes/UserNote'
 import { NEXT_ACTIVITIES_PLACEHOLDER, QUICK_NOTES_PLACEHOLDER } from '@/lib/mock'
+import { Input } from '@/components/ui/input'
 
 const Home = () => {
 
@@ -21,7 +22,6 @@ const Home = () => {
     useEffect(() => {
         grid = GridStack.init({
             marginUnit: 'px',
-            margin: 16,
             handle: '.handle',
             cellHeight: '100px',
             columnOpts: {
@@ -76,26 +76,35 @@ const Home = () => {
                     </div>
                 </div>
                 <div className='grid-stack-item' gs-w="2" gs-h="4" ref={(node) => { const map = getMap(); if (node) map.set('item1', node); else map.delete('item1'); }}>
-                    <div className='grid-stack-item-content bg-white p-4 rounded-md border'>
-                        <div className='flex items-center mb-4 gap-2'>
-                            <GripVertical size={20} className='handle cursor-pointer' />
-                            <h2 className="text-sm font-semibold uppercase tracking-wide text-neutral-700">Notas</h2>
-                        </div>
-                        {QUICK_NOTES_PLACEHOLDER.length === 0 ? (
-                            <div className="lg:col-span-2">
-                                <Card className="flex items-center justify-center h-64 bg-neutral-50 p-4 text-center text-neutral-500 dark:bg-neutral-900/40 shadow-none">
-                                    Nenhuma Nota
-                                </Card>
-                            </div>
-                        ) : (
-                            <div className="lg:col-span-2">
-                                <div className="flex flex-col gap-3">
-                                    {QUICK_NOTES_PLACEHOLDER.map(note => (
-                                        <UserNote key={note.id} {...note} />
-                                    ))}
+                    <div className='grid-stack-item-content rounded-md border bg-white'>
+                        <div className=' h-[calc(100%-96px)] overflow'>
+                            <div className='p-4 flex flex-col gap-2'>
+                                <div className='flex items-center gap-2 w-full'>
+                                    <GripVertical size={20} className='handle cursor-pointer' />
+                                    <h2 className="text-sm font-semibold uppercase tracking-wide text-neutral-700">Notas</h2>
+                                </div>
+                                <div className=''>
+                                <Input placeholder='Pesquisar Notas...' className='shadow-none'/>
                                 </div>
                             </div>
-                        )}
+                            <div className='px-4 pb-4 h-full overflow-y-auto'>
+                                {QUICK_NOTES_PLACEHOLDER.length === 0 ? (
+                                    <div>
+                                        <Card className="flex items-center justify-center h-64 bg-neutral-50 p-4 text-center text-neutral-500 dark:bg-neutral-900/40 shadow-none">
+                                            Nenhuma Nota
+                                        </Card>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <div className="flex flex-col gap-3">
+                                            {QUICK_NOTES_PLACEHOLDER.map(note => (
+                                                <UserNote key={note.id} {...note} />
+                                            ))}
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='grid-stack-item' gs-w="6" gs-h="4" ref={(node) => { const map = getMap(); if (node) map.set('item1', node); else map.delete('item1'); }}>
